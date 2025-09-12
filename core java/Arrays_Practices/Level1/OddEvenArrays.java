24a428d0fbada7a2fa30a8eabe3ab039b2001483
vikash kumar <vikashsingh.cs22@gmail.com>
2025-09-12 15:16:38 +0530

    [Vikash Kumar] feat: add OddEvenArrays.java

---
+++ core java/Arrays_Practices/Level1/OddEvenArrays.java
@@ -0,0 +1,36 @@
+package Arrays_Practices.Level1;
+
+import java.util.Scanner;
+
+public class OddEvenArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a natural number: ");
+        int number = sc.nextInt();
+
+        if (number <= 0) {
+            System.out.println("Error: Please enter a natural number.");
+            return;
+        }
+
+        int[] even = new int[number / 2 + 1];
+        int[] odd = new int[number / 2 + 1];
+        int evenIndex = 0, oddIndex = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0)
+                even[evenIndex++] = i;
+            else
+                odd[oddIndex++] = i;
+        }
+
+        System.out.print("Even numbers: ");
+        for (int i = 0; i < evenIndex; i++)
+            System.out.print(even[i] + " ");
+        
+        System.out.print("\nOdd numbers: ");
+        for (int i = 0; i < oddIndex; i++)
+            System.out.print(odd[i] + " ");
+    }
+}
+
