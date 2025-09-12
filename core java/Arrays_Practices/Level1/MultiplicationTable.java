fae8ea66187c4f3f151a2b2985c7ee8a08fb7ce0
vikash kumar <vikashsingh.cs22@gmail.com>
2025-09-12 15:15:04 +0530

    [Vikash Kumar] feat : add MultiplicationTable.java implementation

---
+++ core java/Arrays_Practices/Level1/MultiplicationTable.java
@@ -0,0 +1,24 @@
+package Arrays_Practices.Level1;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number to print its multiplication table: ");
+        int number = sc.nextInt();
+
+        int[] table = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            table[i] = number * (i + 1);
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(number + " * " + (i + 1) + " = " + table[i]);
+        }
+
+        sc.close();
+    }
+}
