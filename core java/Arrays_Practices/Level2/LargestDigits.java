d811e68dde42d6af01294b880eb4bb0a8e924988
vikash kumar <vikashsingh.cs22@gmail.com>
2025-09-12 15:25:40 +0530

    [Vikash Kumar] feat: add LargestDigits.java

---
+++ core java/Arrays_Practices/Level2/LargestDigits.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class LargestDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+        while (number != 0) {
+            if (index == maxDigit) {
+                System.out.println("Only first 10 digits will be considered.");
+                break;
+            }
+            digits[index] = number % 10;
+            number = number / 10;
+            index++;
+        }
+        int largest = 0;
+        int secondLargest = 0;
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest;
+                largest = digits[i];
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i];
+            }
+        }
+        System.out.println("Largest digit: " + largest);
+        System.out.println("Second largest digit: " + secondLargest);
+    }
+}
